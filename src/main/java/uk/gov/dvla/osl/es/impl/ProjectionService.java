9792151c44b19b8607987204d67aba09dbaae1a3
Peter van der Merwe <peter@vandermerwe.co.za>
2016-02-05 12:03:27 +0000

    Added in new http event store, and a new ApplicationService - renamed the old one to BlockingApplicationService

--- src/main/java/uk/gov/dvla/osl/es/impl/ProjectionService.java
+++ src/main/java/uk/gov/dvla/osl/es/impl/ProjectionService.java
@@ -1,36 +1,25 @@
 package uk.gov.dvla.osl.es.impl;
 
-import uk.gov.dvla.osl.es.api.Command;
 import uk.gov.dvla.osl.es.api.Event;
-import uk.gov.dvla.osl.es.store.memory.EventStore;
-import uk.gov.dvla.osl.es.store.memory.EventStream;
 
-import java.util.List;
+import java.lang.reflect.InvocationTargetException;
 
 public class ProjectionService {
-    private final EventStore eventStore;
-    private CommandHandlerLookup commandHandlerLookup;
+    private Object dataStore;
+    private Class<?> aggregateType;
+    private Object aggregate;
 
-    public ProjectionService(EventStore eventStore, Class<?>... aggregateTypes) {
-        this.eventStore = eventStore;
-        this.commandHandlerLookup = new CommandHandlerLookup(ReflectionUtil.HANDLE_METHOD, aggregateTypes);
+    public ProjectionService(Object dataStore, Class dataStoreType, Class aggregateType) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        this.dataStore = dataStore;
+        this.aggregateType = aggregateType;
+        this.aggregate = aggregateType.getConstructor(dataStoreType).newInstance(dataStore);
     }
 
-    public void handle(Command command) throws Exception {
-        EventStream<Long> eventStream = eventStore.loadEventStream(command.aggregateId());
-        Object target = newAggregateInstance(command);
-        for (Event event : eventStream) {
-            ReflectionUtil.invokeHandleMethod(target, event);
-        }
-        List<Event> events = ReflectionUtil.invokeHandleMethod(target, command);
-        if (events != null && events.size() > 0) {
-            eventStore.store(command.aggregateId(), eventStream.version(), events);
-        } else {
-            // Command generated no events
-        }
+    public void handle(Event event) throws Exception {
+        ReflectionUtil.invokeHandleMethod(aggregate, event);
     }
 
-    private Object newAggregateInstance(Command command) throws InstantiationException, IllegalAccessException {
-        return commandHandlerLookup.targetType(command).newInstance();
+    public void save() {
+        ReflectionUtil.invokeSaveMethod(aggregate);
     }
 }
